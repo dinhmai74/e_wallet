5d0cb567930369a5f0112cd6f71425f26d0864d2
dinhmai74 <dinh.mai@nextfunc.com>
2019-12-03 19:04:15 +0700

    refactor dir

---
+++ app/screens/buy-train-ticket/indext.ts
@@ -0,0 +1,6 @@
+export * from "./fill-info-screen"
+export * from "./chose-time-screen"
+export * from "./select-pos-screen"
+export * from "./buy-train-ticket-screen"
+export * from "./confirm-transaction-screen"
+export * from "./confirm-passenger-info-screen"
